a9687be9938cd145e78788b4e545aef5a97f2f41
Wishvanath Adhikari <buddhiwishvanath123@gmail.com>
2023-08-13 23:15:38 +0530

    Algo-8

--- Algo_7.java
+++ Algo_7.java
@@ -3,41 +3,42 @@ import java.util.Scanner;
 class Algo_7 {
     private final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        String sentence = "Hello java is good programming language";
-        
-
-        char[] sent = sentence.strip().toCharArray();
-        
-        
-        int wordEndCount = 0;
-        for (int i = 0; i < sent.length; i++) {
-            if(sent[i] == ' ' || i == (sent.length -1)) wordEndCount++;
-        }
-       
-        //create string array
-        String[] strarray = new String[wordEndCount];
-        String tempstr = "";
-        int count = 0;
-        for (int i = 0; i < sent.length; i++) {
-            if(sent[i] == ' ' || i == sent.length -1){
-                if(i == sent.length -1) tempstr +=sent[i];
-                strarray[count] = tempstr;
-                count++;
-                tempstr = ""; 
-            }else {
-                tempstr +=sent[i];
+        do{
+            String sentence = scanner.nextLine();
+            if(sentence.isBlank()) {
+                System.out.println("Invalid input");
+                continue;
             }
-        }
-        //find logest and shortest indexes
-        int shortestIndex = 0, longestIndex = 0;
-        for (int i = 0; i < strarray.length-1; i++) {
-            if (strarray[shortestIndex].length() > strarray[i].length()) shortestIndex = i;
-            if (strarray[longestIndex].length() < strarray[i].length()) longestIndex = i;
-            //System.out.println(strarray[i] + " " + strarray[i].length() + " " + shortestIndex);
-            
-        }
-        System.out.println("Shortest word is \"" + strarray[shortestIndex] + "\" has length of " + strarray[shortestIndex].length());
-        System.out.println("Longest word is \"" + strarray[longestIndex] + "\" has length of " + strarray[longestIndex].length());
+            char[] sent = sentence.strip().toCharArray();
+            int wordEndCount = 0;
+            for (int i = 0; i < sent.length; i++) {
+                if(sent[i] == ' ' || i == (sent.length -1)) wordEndCount++;
+            }
+        
+            //create string array
+            String[] strarray = new String[wordEndCount];
+            String tempstr = "";
+            int count = 0;
+            for (int i = 0; i < sent.length; i++) {
+                if(sent[i] == ' ' || i == sent.length -1){
+                    if(i == sent.length -1) tempstr +=sent[i];
+                    strarray[count] = tempstr;
+                    count++;
+                    tempstr = ""; 
+                }else {
+                    tempstr +=sent[i];
+                }
+            }
+            //find logest and shortest indexes
+            int shortestIndex = 0, longestIndex = 0;
+            for (int i = 0; i < strarray.length-1; i++) {
+                if (strarray[shortestIndex].length() > strarray[i].length()) shortestIndex = i;
+                if (strarray[longestIndex].length() < strarray[i].length()) longestIndex = i;
+                //System.out.println(strarray[i] + " " + strarray[i].length() + " " + shortestIndex);
+                
+            }
+            System.out.println("Shortest word is \"" + strarray[shortestIndex] + "\" has length of " + strarray[shortestIndex].length());
+            System.out.println("Longest word is \"" + strarray[longestIndex] + "\" has length of " + strarray[longestIndex].length());
 
 
     }
---
+++ Algo_8.java
@@ -0,0 +1,36 @@
+class Algo_8 {
+    public static void main(String[] args) {
+        
+        int[] numA = {5, 7, -2, 3, 4, 6, -8};
+        int[] numB = {7, 8, 2, 1, -9, 6,};
+
+        //Intersection of sets
+        /*System.out.print("Intersection of numA and numB {");
+        for (int i = 0; i < numA.length; i++) {
+            boolean foundIntersection = false;
+            loop:
+            for (int j = 0; j < numB.length; j++) {
+                if(numA[i] == numB[j] && !foundIntersection) {
+                    foundIntersection = true;
+                    System.out.print(numA[i]+ ", ");
+                }
+            }
+        }*/
+        System.out.println("\b".repeat(2) + "}");
+        
+        //union of set
+        System.out.print("Intersection of numA and numB {");
+        for (int i = 0; i < numA.length; i++) {
+            boolean intersect = false;
+            loop:
+            for (int j = 0; j < numB.length; j++) {
+                if(numA[i] == numB[j] && !intersect) {
+                    intersect = true;
+                    System.out.print(numA[i] + ", ");
+                }else if (numA[i] !=numB[j] && !intersect) System.out.print(numA[i] + ", ");
+                else {continue;}
+            }
+        }
+        System.out.println("\b".repeat(2) + "}");
+    }
+}
